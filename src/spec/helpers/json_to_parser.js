117d6b850a85175eb38ae96acf32996fb4b38fa9
koflTW <kfehrs@thoughtworks.com>
2018-03-21 13:24:42 +0100

    add object and string parser builder
    Co-authored-by: Simon Weiler <sweiler@thoughtworks.com>

--- src/spec/helpers/json_to_parser.js
+++ src/spec/helpers/json_to_parser.js
@@ -1,11 +1,54 @@
-let {parse9} = require('./cobol_to_json');
+let {parse9, parseX} = require('./cobol_to_json');
 
-function buildParser(dataSpecObj) {
-
+let buildIntegerParser = function (dataSpecObj) {
     let length = dataSpecObj["maximum"].toString().length;
     return function (inputStr) {
         return parse9(inputStr, length);
     };
+};
+
+let buildStringParser = function (dataSpecObj) {
+    return function (inputStr) {
+        return parseX(inputStr, dataSpecObj["maxLength"]);
+    };
+};
+
+let buildObjectParser = function (dataSpecObj) {
+
+    const keys = Object.keys(dataSpecObj.properties);
+    return function (inputStr) {
+        let buffer = inputStr;
+
+        function cut(count) {
+            let cutted = buffer.substr(0, count);
+            buffer = buffer.substr(count);
+            return cutted;
+        }
+
+        if (keys.length > 0) {
+            let obj = {};
+            for (let i = 0; i < keys.length; i++) {
+                obj[keys[i]] = buildParser(dataSpecObj.properties[keys[i]])(cut(8));
+            }
+            return obj;
+        }
+        else {
+            return {};
+        }
+    }
+};
+
+function buildParser(dataSpecObj) {
+    switch (dataSpecObj.type) {
+        case 'integer':
+            return buildIntegerParser(dataSpecObj);
+        case 'string':
+            return buildStringParser(dataSpecObj);
+        case 'object':
+            return buildObjectParser(dataSpecObj);
+        default :
+            throw new Error("Not a valid object type");
+    }
 }
 
 module.exports = {
--- src/spec/json_to_parser_spec.js
+++ src/spec/json_to_parser_spec.js
@@ -2,6 +2,11 @@ let {buildParser} = require('./helpers/json_to_parser');
 
 describe('JSON to parser', () => {
 
+    it('fails when given an unrecognized type', () => {
+        expect( () => buildParser({"type":"unknown"}))
+            .toThrowError(Error);
+    });
+
     describe('Digit parser', () => {
         it('creates an integer parser', () => {
 
@@ -29,6 +34,58 @@ describe('JSON to parser', () => {
 
             expect(resultParser("026")).toBe(26);
             expect(resultParser("107")).toBe(107);
+        });
+    });
+
+    describe('String parser', () => {
+        it('creates a string parser', () => {
+
+            let obj = {
+                "type": "string",
+                "maxLength": 5,
+            };
+
+            let resultParser = buildParser(obj);
+
+            expect(resultParser("Hall ")).toBe("Hall");
+        });
+    });
+
+    describe('Object parser', () => {
+        it('creates an object parser for empty objects', () => {
+
+            let obj = {
+                "type": "object",
+                "properties": {},
+            };
+
+            let resultParser = buildParser(obj);
+
+            expect(resultParser("")).toEqual({});
+        });
+
+        it('creates parsers recursively', () => {
+            let obj = {
+                "type": "object",
+                "properties": {
+                    "test_item": {
+                        "type": "string",
+                        "maxLength": 8
+                    },
+                    "test_int": {
+                        "type": "integer",
+                        "maximum": 999,
+                        "minimum": 0
+                    }
+                },
+            };
+
+            let resultParser = buildParser(obj);
+
+            expect(resultParser("Hallo   001")).toEqual({
+                "test_item": "Hallo",
+                "test_int": 1
+            });
         });
     });
 });
